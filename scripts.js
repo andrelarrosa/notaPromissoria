4fd35d0142a369cb42bd250224304122d0755901
andrelarrosa <andre_luiz_2001@hotmail.com>
2022-04-29 18:24:04 -0300

    validações e alterando inputs

--- scripts.js
+++ scripts.js
@@ -8,6 +8,10 @@ function anoParaExtenso(data) {
 }
 
 function carregarNota() {
+    if(document.getElementById('nomeDoCredor').value == '' || (document.getElementById('cpfCredor').value == '' && document.getElementById('cnpjCredor').value == '') || document.getElementById('valorNota').value == '' || document.getElementById('primeiroVencimento').value == '' || document.getElementById('dataEmissao').value == '' || document.getElementById('nomeDevedor').value=='' || (document.getElementById('cpfDevedor').value == '' && document.getElementById('cnpjDevedor').value == '') || document.getElementById('cep').value == '' || document.getElementById('rua').value == '' || document.getElementById('cidade').value == '' || document.getElementById('estado').value == '' || document.getElementById('numero').value == '' || document.getElementById('bairro').value == '' || document.getElementById('referencia').value == ''){
+        alert("Preencha todos os campos!");
+        return;
+    }
     var nota = '';
     var nomeCredor = document.getElementById('nomeDoCredor').value;
     var cpfCnpjCredor = document.getElementById('cpfCredor').value != '' ? document.getElementById('cpfCredor').value : document.getElementById('cnpjCredor').value;
@@ -23,21 +27,21 @@ function carregarNota() {
     var numero = document.getElementById('numero').value;
     var bairro = document.getElementById('bairro').value;
     var referencia = document.getElementById('referencia').value;
-    var quantidadeNotas = document.getElementById('quantidade').value != ''? document.getElementById('quantidade').value:1;
-    for(var i = 1; i<=quantidadeNotas; i++){
+    var quantidadeNotas = document.getElementById('quantidade').value != '' ? document.getElementById('quantidade').value : 1;
+    for (var i = 1; i <= quantidadeNotas; i++) {
         nota += ' <div class="borda">\
         <p style="text-align: center;">\
-            <b>NOTA PROMISSÓRIA (Nº'+i+' de '+quantidadeNotas+')</b>\
+            <b>NOTA PROMISSÓRIA (Nº'+ i + ' de ' + quantidadeNotas + ')</b>\
         </p>\
         <p>Vencimento: <b>'+ primeiroVencimento + '</b>.</p>\
         <p><b>R$ '+ extenso(valorNota) + '</b></p>\
-        <p>No dia <b >'+ primeiroVencimento + '</b> pagarei por esta única via de NOTA PROMISSÓRIA a <b class="background">' + nomeCredor + '</b>, CPF (ou CNPJ) nº ' + cpfCnpjCredor + ' ou a sua ordem a quantia de '+extenso(valorNota)+' reais.</p>\
-        <p>Pagável na Rua '+rua+', nº '+numero+' - '+bairro+' na cidade de '+cidade+' - '+estado+'.</p>\
-        <p>Emitente: '+ nomeDevedor + ', CPF (ou CNPJ) nº ' + cpfCnpjDevedor + ', com endereço à Rua '+rua+', nº '+numero+' - '+bairro+' na cidade de '+cidade+' - '+estado+' - CEP '+cep+'.</p>\
-        <p>'+cidade+' - '+estado+', '+emissao+'.</p>\
-        <p>Referência: '+referencia+'</p>\
+        <p>No dia <b >'+ primeiroVencimento + '</b> pagarei por esta via de NOTA PROMISSÓRIA a <b class="background">' + nomeCredor + '</b>, CPF (ou CNPJ) nº ' + cpfCnpjCredor + ' ou a sua ordem a quantia de ' + extenso(valorNota) + ' reais.</p>\
+        <p>Pagável na Rua '+ rua + ', nº ' + numero + ' - ' + bairro + ' na cidade de ' + cidade + ' - ' + estado + '.</p>\
+        <p>Emitente: '+ nomeDevedor + ', CPF (ou CNPJ) nº ' + cpfCnpjDevedor + ', com endereço à Rua ' + rua + ', nº ' + numero + ' - ' + bairro + ' na cidade de ' + cidade + ' - ' + estado + ' - CEP ' + cep + '.</p>\
+        <p>'+ cidade + ' - ' + estado + ', ' + emissao + '.</p>\
+        <p>Referência: '+ referencia + '</p>\
         <hr style="margin-left: 10%; margin-right: 10%; margin-top: 10%;">\
-        <p style="text-align: center;">'+nomeDevedor+'</p>\
+        <p style="text-align: center;">'+ nomeDevedor + '</p>\
     </div>\
     <br>';
     }
@@ -50,138 +54,135 @@ function carregarNota() {
 function extenso(vlr) {
 
     var Num = parseFloat(vlr);
-    if (vlr == 0) {
-        document.getElementById('teste').value = "zero";
-    } else {
-        var inteiro = parseInt(vlr); // parte inteira do valor
-        if (inteiro < 1000000000000000) {
+    var inteiro = parseInt(vlr); // parte inteira do valor
+    if (inteiro < 1000000000000000) {
 
-            var resto = Num.toFixed(2) - inteiro.toFixed(2);       // parte fracionária do valor
-            resto = resto.toFixed(2)
-            var vlrS = inteiro.toString();
+        var resto = Num.toFixed(2) - inteiro.toFixed(2);       // parte fracionária do valor
+        resto = resto.toFixed(2)
+        var vlrS = inteiro.toString();
 
-            var cont = vlrS.length;
-            var extenso = "";
-            var auxnumero;
-            var auxnumero2;
-            var auxnumero3;
+        var cont = vlrS.length;
+        var extenso = "";
+        var auxnumero;
+        var auxnumero2;
+        var auxnumero3;
 
-            var unidade = ["", "UM", "DOIS", "TRÊS", "QUATRO", "CINCO",
-                "SEIS", "SETE", "OITO", "NOVE", "DEZ", "ONZE",
-                "DOZE", "TREZE", "QUATORZE", "QUINZE", "DEZESSEIS",
-                "DEZESSETE", "DEZOITO", "DEZENOVE"];
+        var unidade = ["", "UM", "DOIS", "TRÊS", "QUATRO", "CINCO",
+            "SEIS", "SETE", "OITO", "NOVE", "DEZ", "ONZE",
+            "DOZE", "TREZE", "QUATORZE", "QUINZE", "DEZESSEIS",
+            "DEZESSETE", "DEZOITO", "DEZENOVE"];
 
-            var centena = ["", "CENTO", "DUZENTOS", "TREZENTOS",
-                "QUATROCENTOS", "QUINHENTOS", "SEISCENTOS",
-                "SETECENTOS", "OITOCENTOS", "NOVECENTOS"];
+        var centena = ["", "CENTO", "DUZENTOS", "TREZENTOS",
+            "QUATROCENTOS", "QUINHENTOS", "SEISCENTOS",
+            "SETECENTOS", "OITOCENTOS", "NOVECENTOS"];
 
-            var dezena = ["", "", "VINTE", "TRINTA", "QUANRENTA", "CINQUENTA",
-                "SESSENTA", "SETENTA", "OITENTA", "NOVENTA"];
+        var dezena = ["", "", "VINTE", "TRINTA", "QUANRENTA", "CINQUENTA",
+            "SESSENTA", "SETENTA", "OITENTA", "NOVENTA"];
 
-            var qualificaS = ["REAIS", "MIL", "MILHÃO", "BILHÃO", "TRILHÃO"];
-            var qualificaP = ["REAIS", "MIL", "MILHÕES", "BILHÕES", "TRILHÕES"];
+        var qualificaS = ["REAIS", "MIL", "MILHÃO", "BILHÃO", "TRILHÃO"];
+        var qualificaP = ["REAIS", "MIL", "MILHÕES", "BILHÕES", "TRILHÕES"];
 
-            for (var i = cont; i > 0; i--) {
-                var verifica1 = "";
-                var verifica2 = 0;
-                var verifica3 = 0;
-                auxnumero2 = "";
-                auxnumero3 = "";
-                auxnumero = 0;
-                auxnumero2 = vlrS.substr(cont - i, 1);
+        for (var i = cont; i > 0; i--) {
+            var verifica1 = "";
+            var verifica2 = 0;
+            var verifica3 = 0;
+            auxnumero2 = "";
+            auxnumero3 = "";
+            auxnumero = 0;
+            auxnumero2 = vlrS.substr(cont - i, 1);
+            auxnumero = parseInt(auxnumero2);
+
+
+            if ((i == 14) || (i == 11) || (i == 8) || (i == 5) || (i == 2)) {
+                auxnumero2 = vlrS.substr(cont - i, 2);
                 auxnumero = parseInt(auxnumero2);
+            }
 
+            if ((i == 15) || (i == 12) || (i == 9) || (i == 6) || (i == 3)) {
+                extenso = extenso + centena[auxnumero];
+                auxnumero2 = vlrS.substr(cont - i + 1, 1)
+                auxnumero3 = vlrS.substr(cont - i + 2, 1)
 
-                if ((i == 14) || (i == 11) || (i == 8) || (i == 5) || (i == 2)) {
-                    auxnumero2 = vlrS.substr(cont - i, 2);
+                if ((auxnumero2 != "0") || (auxnumero3 != "0"))
+                    extenso += " e ";
+
+            } else
+
+                if (auxnumero > 19) {
+                    auxnumero2 = vlrS.substr(cont - i, 1);
                     auxnumero = parseInt(auxnumero2);
-                }
+                    extenso = extenso + dezena[auxnumero];
+                    auxnumero3 = vlrS.substr(cont - i + 1, 1)
 
-                if ((i == 15) || (i == 12) || (i == 9) || (i == 6) || (i == 3)) {
-                    extenso = extenso + centena[auxnumero];
-                    auxnumero2 = vlrS.substr(cont - i + 1, 1)
-                    auxnumero3 = vlrS.substr(cont - i + 2, 1)
-
-                    if ((auxnumero2 != "0") || (auxnumero3 != "0"))
+                    if ((auxnumero3 != "0") && (auxnumero2 != "1"))
                         extenso += " e ";
-
-                } else
-
-                    if (auxnumero > 19) {
-                        auxnumero2 = vlrS.substr(cont - i, 1);
-                        auxnumero = parseInt(auxnumero2);
-                        extenso = extenso + dezena[auxnumero];
-                        auxnumero3 = vlrS.substr(cont - i + 1, 1)
-
-                        if ((auxnumero3 != "0") && (auxnumero2 != "1"))
-                            extenso += " e ";
-                    } else if ((auxnumero <= 19) && (auxnumero > 9) && ((i == 14) || (i == 11) || (i == 8) || (i == 5) || (i == 2))) {
+                } else if ((auxnumero <= 19) && (auxnumero > 9) && ((i == 14) || (i == 11) || (i == 8) || (i == 5) || (i == 2))) {
+                    extenso = extenso + unidade[auxnumero];
+                } else if ((auxnumero < 10) && ((i == 13) || (i == 10) || (i == 7) || (i == 4) || (i == 1))) {
+                    auxnumero3 = vlrS.substr(cont - i - 1, 1);
+                    if ((auxnumero3 != "1") && (auxnumero3 != ""))
                         extenso = extenso + unidade[auxnumero];
-                    } else if ((auxnumero < 10) && ((i == 13) || (i == 10) || (i == 7) || (i == 4) || (i == 1))) {
-                        auxnumero3 = vlrS.substr(cont - i - 1, 1);
-                        if ((auxnumero3 != "1") && (auxnumero3 != ""))
-                            extenso = extenso + unidade[auxnumero];
-                    }
+                }
 
-                if (i % 3 == 1) {
-                    verifica3 = cont - i;
-                    if (verifica3 == 0)
-                        verifica1 = vlrS.substr(cont - i, 1);
+            if (i % 3 == 1) {
+                verifica3 = cont - i;
+                if (verifica3 == 0)
+                    verifica1 = vlrS.substr(cont - i, 1);
 
-                    if (verifica3 == 1)
-                        verifica1 = vlrS.substr(cont - i - 1, 2);
+                if (verifica3 == 1)
+                    verifica1 = vlrS.substr(cont - i - 1, 2);
 
-                    if (verifica3 > 1)
-                        verifica1 = vlrS.substr(cont - i - 2, 3);
+                if (verifica3 > 1)
+                    verifica1 = vlrS.substr(cont - i - 2, 3);
 
-                    verifica2 = parseInt(verifica1);
+                verifica2 = parseInt(verifica1);
 
-                    if (i == 13) {
-                        if (verifica2 == 1) {
-                            extenso = extenso + " " + qualificaS[4] + " ";
-                        } else if (verifica2 != 0) { extenso = extenso + " " + qualificaP[4] + " "; }
-                    }
-                    if (i == 10) {
-                        if (verifica2 == 1) {
-                            extenso = extenso + " " + qualificaS[3] + " ";
-                        } else if (verifica2 != 0) { extenso = extenso + " " + qualificaP[3] + " "; }
-                    }
-                    if (i == 7) {
-                        if (verifica2 == 1) {
-                            extenso = extenso + " " + qualificaS[2] + " ";
-                        } else if (verifica2 != 0) { extenso = extenso + " " + qualificaP[2] + " "; }
-                    }
-                    if (i == 4) {
-                        if (verifica2 == 1) {
-                            extenso = extenso + " " + qualificaS[1] + " ";
-                        } else if (verifica2 != 0) { extenso = extenso + " " + qualificaP[1] + " "; }
-                    }
-                    if (i == 1) {
-                        if (verifica2 == 1) {
-                            extenso = extenso + " " + qualificaS[0] + " ";
-                        } else { extenso = extenso + " " + qualificaP[0] + " "; }
-                    }
+                if (i == 13) {
+                    if (verifica2 == 1) {
+                        extenso = extenso + " " + qualificaS[4] + " ";
+                    } else if (verifica2 != 0) { extenso = extenso + " " + qualificaP[4] + " "; }
+                }
+                if (i == 10) {
+                    if (verifica2 == 1) {
+                        extenso = extenso + " " + qualificaS[3] + " ";
+                    } else if (verifica2 != 0) { extenso = extenso + " " + qualificaP[3] + " "; }
+                }
+                if (i == 7) {
+                    if (verifica2 == 1) {
+                        extenso = extenso + " " + qualificaS[2] + " ";
+                    } else if (verifica2 != 0) { extenso = extenso + " " + qualificaP[2] + " "; }
+                }
+                if (i == 4) {
+                    if (verifica2 == 1) {
+                        extenso = extenso + " " + qualificaS[1] + " ";
+                    } else if (verifica2 != 0) { extenso = extenso + " " + qualificaP[1] + " "; }
+                }
+                if (i == 1) {
+                    if (verifica2 == 1) {
+                        extenso = extenso + " " + qualificaS[0] + " ";
+                    } else { extenso = extenso + " " + qualificaP[0] + " "; }
                 }
             }
-            resto = resto * 100;
-            var aexCent = 0;
-            if (resto <= 19 && resto > 0)
+        }
+        resto = resto * 100;
+        var aexCent = 0;
+        if (resto <= 19 && resto > 0)
+            extenso += " e " + unidade[resto] + " Centavos";
+        if (resto > 19) {
+            aexCent = parseInt(resto / 10);
+
+            extenso += " e " + dezena[aexCent]
+            resto = resto - (aexCent * 10);
+
+            if (resto != 0)
                 extenso += " e " + unidade[resto] + " Centavos";
-            if (resto > 19) {
-                aexCent = parseInt(resto / 10);
-
-                extenso += " e " + dezena[aexCent]
-                resto = resto - (aexCent * 10);
-
-                if (resto != 0)
-                    extenso += " e " + unidade[resto] + " Centavos";
-                else extenso += " Centavos";
-            }
-
-            return extenso
+            else extenso += " Centavos";
         }
 
+        return extenso
     }
+
+
 }
 
 function mascaras(i, t) {
